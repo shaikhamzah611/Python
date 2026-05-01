0047b1bbf7ce7f689187626fd1ccd9c919e06c9f
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-05-01 18:39:12 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,12 @@
+
+birthday1 = (2011, 11, 6)   # Year,Month,Day
+birthday2 = (1985, 12, 25)
+birthday3 = (2000, 7, 4)
+birthday4 = (1995, 3, 18)
+birthday5 = (2010, 11, 30)
+
+print("birthday1=",birthday1)
+print("birthday2=",birthday2)
+print("birthday3=",birthday3)
+print("birthday4=",birthday4)
+        
