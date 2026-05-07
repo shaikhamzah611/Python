cca62961d673c93c263339d0de9e591034729dd3
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-05-07 10:22:03 +0530

    Add files via upload

---
+++ dailyroutine.py
@@ -0,0 +1,12 @@
+
+print("My Daily Routine")
+
+print("I wake up at 7 AM.")
+
+print("I go to school at 8 AM.")
+
+print("I play with my friends in the evening.")
+
+print("I study for 3 hours at night.")
+
+print("Total study hours in a week:", 3 * 7)
