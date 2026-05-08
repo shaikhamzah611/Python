f44a2aa5dbc970808cf497e5c8105772e37c1638
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-05-08 18:05:01 +0530

    Add files via upload

---
+++ greaterorsmaller.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number:"))
+if (i < 15):
+    print("i is less than 15")
+    print("i'm in if block")
+else:
+    print("i is greater than 15")
+    print("i'm in else block")
+print("i'm in if block and  not in else block")
