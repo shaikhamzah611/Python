7eba955c164c53cc62359bcca9fb138705535155
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-05-08 21:57:49 +0530

    Add files via upload

---
+++ weatherclothes.py
@@ -0,0 +1,9 @@
+temperature = int(input("What is the current Temperature in celsius:"))
+
+if temperature < 20:
+    print("Jacket and Pullover are required because it is cold outdoor")
+else:
+    temperature > 30
+    print("Light and Soft clothes are perfect")  
+if (temperature > 20 and temperature < 30):
+    print("A Simple jacket and light clothes is Good")
