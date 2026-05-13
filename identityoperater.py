e6fb9cd0c92099c0e8c263757957e6b4af343075
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-05-13 17:19:53 +0530

    Add files via upload

---
+++ identityoperater.py
@@ -0,0 +1,26 @@
+x = 5
+
+if (type(x) is int):
+    print("True")
+
+else:
+    print("False")
+
+x = 5.5
+
+if (type(x) is not float):
+    print("True")
+
+else:
+    print("False")
+
+x = 20
+y = 20
+
+if (x is y):
+    print("x&y SAME identity")
+
+y = 30
+
+if (x is not y):
+    print("x&y DIFFERENT identity")
