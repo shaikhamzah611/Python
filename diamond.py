e4e045b84a6abeef9c3dc88d28137f8892ee1dd0
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-06-08 18:50:43 +0530

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,19 @@
+rows = int(input("Enter the number of rows:"))
+
+for i in range(1,rows+1):
+    
+    for j in range(rows-i):
+        print(" ",end="")
+
+    for j in range(2*i-1):
+        print("*",end="")
+    print()
+
+for i in range(rows - 1,0,-1):
+
+    for j in range(rows-i):
+        print(" ",end="")
+
+    for j in range(2*i-1):
+        print("*",end="")
+    print()
