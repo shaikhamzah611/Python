748fbaade59979ac11c357c769edd5d7ef2216d4
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-06-24 18:35:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+empty_list=[]
+print(empty_list)
+
+fruits = ["apple","banana","orange"]
+fruits = fruits[::-1]
+print(fruits)
+
+four = [1,2,3]*4
+print(four)
+
+list = [350,3.5,"blue"]
+list = list[::-1]
+print(list)
