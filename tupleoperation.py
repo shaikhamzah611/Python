99921402ea2f0f890264e1b2d1a324278ec0f899
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-06-26 18:43:18 +0530

    Add files via upload

---
+++ tupleoperation.py
@@ -0,0 +1,19 @@
+tuplex = (tuple,False,3.2,1)
+print(tuplex)
+
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+
+tuplex=tuplex+(9,)
+print(tuplex)
+
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+
+tuplex = (2,1,3,4,5,6,8,7,9)
+
+_slice = tuplex[3:5]
+print(_slice)
+
+_slice = tuplex[:6]
+print(_slice)
