8936cf61c9a6d870f0def7ce6e04084c727c39b3
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-06-30 18:40:36 +0530

    Add files via upload

---
+++ ops_set.py
@@ -0,0 +1,18 @@
+mset = {1,2,3}
+print(mset)
+
+mset = {4.6,"Hello",(1,7,8)}
+print(mset)
+
+mset = {1,2,3,4,3,2,9,9}
+print(mset)
+
+mset = set([1,2,3])
+print(mset,"\n")
+
+num_set = set([23,4,5,1,9,7,7])
+print("The Original Set is: ")
+print(num_set)
+num_set.pop()
+print("After removing the first element:")
+print(num_set,"\n")
