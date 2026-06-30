c455be1d767d8efb9808a5801a6d16d20ed076ab
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-06-30 18:47:57 +0530

    Add files via upload

---
+++ inter.py
@@ -0,0 +1,9 @@
+setx = {"Black","White","Green"}
+sety = {"Green","Blue","Red"}
+print("The Original Sets are:")
+print(setx)
+print(sety)
+
+print("\nThe intersection of set is:")
+setz = setx.intersection(sety)
+print(setz)
