d90f5f0db9e59b4f26bc59d43629a378b2157fe6
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-07-05 12:34:38 +0530

    Add files via upload

---
+++ symetric.py
@@ -0,0 +1,11 @@
+print("\nExample A:")
+set1 = {"blue","green"}
+set2 = {"blue","yellow"}
+
+print(set1.symmetric_difference(set2))
+
+print("\nExample B: ")
+Set1 = {1,2,3,4,5}
+Set2 = {1,5,6,7,8,9}
+
+print(Set1.symmetric_difference(Set2))
