cdc0c7e27e32c74fde018de6399f0355e82ca984
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-07-06 18:55:37 +0530

    Add files via upload

---
+++ pythontest3.py
@@ -0,0 +1,10 @@
+grade = {"Joe","David","Brayn","Rohit","Lily"}
+score = {90,92,89,95,84}
+total = 90+92+89+95+84
+for i in range(1):
+    avg = total/100
+    print("Average of class is: ",total)
+print("Highest score is: ")
+print(max(score))
+print("Lowest score is: ")
+print(min(score))
