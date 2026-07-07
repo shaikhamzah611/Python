e139e3dd43813414c35e0a5046195f9deaf50a77
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-07-07 20:07:18 +0530

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,9 @@
+class car:
+    def __init__(self,Topspeed,Mileage):
+        self.Topspeed = Topspeed
+        self.Mileage = Mileage
+
+CarX = car(300,20)
+
+print("The Topseed of the car: ",CarX.Topspeed)
+print("The Mileage: ",CarX.Mileage)
