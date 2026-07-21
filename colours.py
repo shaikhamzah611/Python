13cecb528c18479450cf47103c22eefc0691c95a
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-07-21 22:11:17 +0530

    Increase screen size from 500x500 to 600x600

--- colours.py
+++ colours.py
@@ -2,7 +2,7 @@ import pygame
 
 def main():
     pygame.init()
-    screen_width, screen_height = 500, 500
+    screen_width, screen_height = 600, 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption('color changing sprite')
 
