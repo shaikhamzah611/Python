76a7440529c15745d597fad58818074acca545e1
shaikhamzah611 <shaikhamzah611@gmail.com>
2026-07-28 15:27:54 +0530

    Add files via upload

---
+++ intetorom.py
@@ -0,0 +1,32 @@
+class Roman:
+    def convert(self, num):
+        values = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        ]
+
+        result = ""
+
+        for value, symbol in values:
+            while num >= value:
+                result += symbol
+                num -= value
+
+        return result
+
+
+number = int(input("Enter an integer: "))
+
+obj = Roman()
+print("Roman numeral:", obj.convert(number))
